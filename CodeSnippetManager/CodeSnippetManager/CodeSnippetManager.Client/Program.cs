ca37546f098d5dc5f1920f988c220ac0cb7586ef
Xobot77 <jwfranklin@hotmail.co.uk>
2024-09-01 09:23:23 +0100

    New request style

--- CodeSnippetManager/CodeSnippetManager/CodeSnippetManager.Client/Program.cs
+++ CodeSnippetManager/CodeSnippetManager/CodeSnippetManager.Client/Program.cs
@@ -3,5 +3,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Options;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient(Options.DefaultName, httpClient =>
+{
+	httpClient.BaseAddress = new Uri("http://localhost:5194/");
+});
 await builder.Build().RunAsync();
--- CodeSnippetManager/CodeSnippetManager/CodeSnippetManager/Program.cs
+++ CodeSnippetManager/CodeSnippetManager/CodeSnippetManager/Program.cs
@@ -2,6 +2,7 @@ using CodeSnippetManager.Client.Pages;
 using CodeSnippetManager.Components;
 using CodeSnippetManager.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -15,7 +16,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient(Options.DefaultName, httpClient =>
+{
+    httpClient.BaseAddress = new Uri("http://localhost:5194/");
+});
 
 builder.Services.AddCors(options =>
 {
