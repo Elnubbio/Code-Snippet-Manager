08fa2b4f1a8198cf1dc71b76a721de83696c04d8
Dan <elnubbio@hotmail.com>
2024-09-01 10:04:45 +0100

    migs

--- CodeSnippetManager/CodeSnippetManager/CodeSnippetManager/Data/ApplicationDbContext.cs
+++ CodeSnippetManager/CodeSnippetManager/CodeSnippetManager/Data/ApplicationDbContext.cs
@@ -11,4 +11,5 @@ public class ApplicationDbContext : DbContext
 	}
 	
 	public DbSet<Snippet> Snippets { get; set; }
+    public DbSet<Tag> Tags { get; set; }
 }
---
+++ CodeSnippetManager/CodeSnippetManager/CodeSnippetManager/Migrations/20240901085845_NewTableTags.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeSnippetManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewTableTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TagId",
+                table: "Snippets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Colour = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Snippets_TagId",
+                table: "Snippets",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Snippets_Tags_TagId",
+                table: "Snippets",
+                column: "TagId",
+                principalTable: "Tags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Snippets_Tags_TagId",
+                table: "Snippets");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Snippets_TagId",
+                table: "Snippets");
+
+            migrationBuilder.DropColumn(
+                name: "TagId",
+                table: "Snippets");
+        }
+    }
+}
