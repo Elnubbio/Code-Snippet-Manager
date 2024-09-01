0222f31b21217feec19d150a41eec9c23d2e646f
Dan <elnubbio@hotmail.com>
2024-09-01 13:38:02 +0100

    Can now delete tags

--- CodeSnippetManager/CodeSnippetManager/CodeSnippetManager.Client/Data/Models/Tag.cs
+++ CodeSnippetManager/CodeSnippetManager/CodeSnippetManager.Client/Data/Models/Tag.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }    
-        public string Colour { get; set; }
+        public string? Colour { get; set; }
         public IEnumerable<Snippet>? Snippets { get; set; }
     }
 }
